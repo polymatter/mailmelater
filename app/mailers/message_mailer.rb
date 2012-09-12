709694e00cc4a2c7b16efc547dcaf7d9b043743f
Jeffrey Lake <jlake.jobs@gmail.com>
2012-09-12 17:40:22 +0100

    cleaning up

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,7 +2,7 @@ class MessageMailer < ActionMailer::Base
   default from: "from@example.com"
   
   def future_message(message)
-	@message = message
+	@message = message.body
     mail(:to => message.send_to, :subject => message.subject)
   end
 end
--- lib/tasks/mailer.rake
+++ lib/tasks/mailer.rake
@@ -1,6 +1,4 @@
 desc "post a test email"
 task :post_mail => :environment do
-  puts "hi there"
   Message.post_mail
-  puts "bye"
 end
